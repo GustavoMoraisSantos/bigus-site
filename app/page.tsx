a69a3f3e248d8fa3a6e1932023e92369d089d10c
GabriellCarvalhoo <gferreiracarvalho26@gmail.com>
2025-03-31 15:51:20 -0300

    update page title to reflect recent changes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="min-h-screen pt-20 bg-gray-50">
     <Navbar />
     <div className="text-center py-20 px-4">
-      <h1 className="text-xl md:text-4xl font-bold mb-4">Catálogo de Produtos de Limpeza</h1>
+      <h1 className="text-xl md:text-4xl font-bold mb-4">Catálogo de Produtos de Limpeza - atualizado</h1>
       <p className="text-lg">Explore nossa variedade de produtos de limpeza.</p>
     </div>
 
